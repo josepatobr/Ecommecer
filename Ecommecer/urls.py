5cc302ad627f04cc290a2778f9708a6a925908ae
josepatobr <169946707+josepatobr@users.noreply.github.com>
2025-09-22 17:25:01 -0300

    arrumando pequenos bugs

--- Ecommecer/urls.py
+++ Ecommecer/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Ecommecer import views
+from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from cadastro import views
+from . import views
 
 
 urlpatterns = [
